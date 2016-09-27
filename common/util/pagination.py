7cd0f019b04fbbc83d38e6932c6877e1e9606727
ChenHao <275258836@qq.com>
2016-09-27 13:31:29 +0800

    增加条件过滤

--- common/util/pagination.py
+++ common/util/pagination.py
@@ -10,7 +10,7 @@ class Pagination(object):
     def create_pagination(self, from_name='', model_name='',
                           cur_page=1, start_page_omit_symbol = '...',
                           end_page_omit_symbol = '...', one_page_data_size=10,
-                          show_page_item_len=9):
+                          show_page_item_len=9, where_dict={}):
         """通过给的model和分页参数对相关model进行分页
         Args: 
             from_name: 导入模块的 from后面的参数
@@ -24,6 +24,7 @@ class Pagination(object):
                 1 2 3 4 ...
             one_page_data_size: 每一页显示几行
             show_page_item_len: 显示几个能点击的页数
+            where_dict: filter 过滤的条件
         Return: 
             pagination: dict
                     pagination = { 
@@ -51,72 +52,105 @@ class Pagination(object):
         # 导入模块
         exec import_str
 
-        start_pos = (cur_page - 1) * one_page_data_size  
-        end_pos = start_pos + one_page_data_size  
 
-        # 查找需要的model数据
-        find_objs_str = ('{model_name}.objects.all()'
-                         '[{start_pos}:{end_pos}]'.format(
-                                               model_name = model_name,
-                                               start_pos = start_pos,
-                                               end_pos = end_pos))
-        objs = eval(find_objs_str)  
-  
-        # 计算总共的页数
-        find_objs_count_str = '{model_name}.objects.count()'.format(
-                                               model_name = model_name)
-        all_obj_counts = eval(find_objs_count_str)
-        all_page = all_obj_counts / one_page_data_size
-        remain_obj = all_obj_counts % one_page_data_size
-        if remain_obj > 0:
-            all_page += 1
+        # 定义Django查询字符串
+        find_objs_str = "{model_name}.objects{filter_str}{op}".format(
+                                                   model_name = model_name,
+                                                   filter_str = '{filter_str}',
+                                                   op = '{op}')
+        # 初始化过滤条件字符串
+        find_objs_filter_str = find_objs_str
+        # 过滤 查询条件
+        if where_dict: # 有 WHERE 条件
+            filter_str_list = []
+            for key, value in where_dict.iteritems():
+                if isinstance(value, str): value = "'{value}'".format(value=value)
+                filter_str = ".filter({key}={value})".format(key = key, 
+                                                             value = value)
+                filter_str_list.append(filter_str)
 
-        # 限制当前页不能小于1和并且大于总页数
-        cur_page = 1 if cur_page < 1 else cur_page
-        cur_page = all_page if cur_page > all_page else cur_page
-
-        # 获得显示页数的最小页
-        start_page = cur_page - show_page_item_len / 2
-        if start_page > all_page - show_page_item_len:
-            start_page = all_page - show_page_item_len + 1
-        start_page = 1 if start_page < 1 else start_page
-
-        # 获得显示页数的最大页
-        end_page = cur_page + show_page_item_len / 2
-        end_page = all_page if end_page > all_page else end_page
-        if end_page < show_page_item_len and all_page > show_page_item_len:
-            end_page = show_page_item_len
-
-        # 获得上一页
-        pre_page = cur_page - 1
-        pre_page = 1 if pre_page < 1 else pre_page
-
-        # 获得下一页
-        next_page = cur_page + 1
-        next_page = all_page if next_page > all_page else next_page
-
-        # 处理省略符，是否显示
-        if start_page <= 1:
-            start_page_omit_symbol = ''
+            find_objs_filter_str = find_objs_filter_str.format(
+                                        filter_str = ''.join(filter_str_list),
+                                        op = '{op}')
+        else: # 没有where条件
+            find_objs_filter_str = find_objs_filter_str.format(
+                                                    filter_str='',
+                                                    op = '{op}')
         
-        if end_page >= all_page:
-            end_page_omit_symbol = ''
+        # 计算总共的页数
+        find_objs_count_str = find_objs_filter_str.format(op = '.count()')
 
-        # 创建能点击的展示页码
-        page_items = range(start_page, end_page + 1)
+        # 运行查询数量
+        all_obj_counts = eval(find_objs_count_str)
 
-        pagination = {
-            'objs': objs,
-            'all_obj_counts': all_obj_counts,
-            'start_pos': start_pos,
-            'end_pos': end_pos,
-            'all_page': all_page,
-            'cur_page': cur_page,
-            'pre_page': pre_page,
-            'next_page': next_page,
-            'page_items': page_items,
-            'start_page_omit_symbol': start_page_omit_symbol,
-            'end_page_omit_symbol': end_page_omit_symbol,
-        }
+        # 有数据才执行分页
+        if all_obj_counts:
+            all_page = all_obj_counts / one_page_data_size
+            remain_obj = all_obj_counts % one_page_data_size
+            if remain_obj > 0:
+                all_page += 1
+
+            # 限制当前页不能小于1和并且大于总页数
+            cur_page = 1 if cur_page < 1 else cur_page
+            cur_page = all_page if cur_page > all_page else cur_page
+
+            # 计算分页开始和结束
+            start_pos = (cur_page - 1) * one_page_data_size  
+            end_pos = start_pos + one_page_data_size  
+
+            # 查找需要的model数据
+            find_objs_page_str = find_objs_filter_str.format(
+                             op = '.all()[{start_pos}:{end_pos}]')
+            # 分页字符串
+            find_objs_str = find_objs_page_str.format(start_pos = start_pos,
+                                                      end_pos = end_pos)
+            # 运行查找分页数据
+            objs = eval(find_objs_str)  
+  
+            # 获得显示页数的最小页
+            start_page = cur_page - show_page_item_len / 2
+            if start_page > all_page - show_page_item_len:
+                start_page = all_page - show_page_item_len + 1
+            start_page = 1 if start_page < 1 else start_page
+
+            # 获得显示页数的最大页
+            end_page = cur_page + show_page_item_len / 2
+            end_page = all_page if end_page > all_page else end_page
+            if end_page < show_page_item_len and all_page > show_page_item_len:
+                end_page = show_page_item_len
+
+            # 获得上一页
+            pre_page = cur_page - 1
+            pre_page = 1 if pre_page < 1 else pre_page
+
+            # 获得下一页
+            next_page = cur_page + 1
+            next_page = all_page if next_page > all_page else next_page
+
+            # 处理省略符，是否显示
+            if start_page <= 1:
+                start_page_omit_symbol = ''
+            
+            if end_page >= all_page:
+                end_page_omit_symbol = ''
+
+            # 创建能点击的展示页码
+            page_items = range(start_page, end_page + 1)
+
+            pagination = {
+                'objs': objs,
+                'all_obj_counts': all_obj_counts,
+                'start_pos': start_pos,
+                'end_pos': end_pos,
+                'all_page': all_page,
+                'cur_page': cur_page,
+                'pre_page': pre_page,
+                'next_page': next_page,
+                'page_items': page_items,
+                'start_page_omit_symbol': start_page_omit_symbol,
+                'end_page_omit_symbol': end_page_omit_symbol,
+            }
+        else: # 没有数据放回 None
+            pagination = None
 
         return pagination
